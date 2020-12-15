fdc29bf223f07f9844882e227c12c958d237b812
afajansari <afajansari@gmail.com>
2020-12-16 00:06:16 +0530

    add listing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  redirectTo: 'lisitngs',
+  pathMatch: 'Full'
+},
+{
+  path: 'listings',
+  loadChildren: () => import('./listing/listing.module').then(m => m.ListingModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
